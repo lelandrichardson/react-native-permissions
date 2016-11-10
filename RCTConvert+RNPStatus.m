72334e609b39cc8d1729db27bebcfa278ed4a046
Haitao Li <haitao.li@airbnb.com>
2016-11-10 14:18:37 -0800

    Remove all permission checks except camera and photo.

--- RCTConvert+RNPStatus.m
+++ RCTConvert+RNPStatus.m
@@ -11,17 +11,9 @@
 @implementation RCTConvert (RNPStatus)
 
 
-RCT_ENUM_CONVERTER(RNPType, (@{ @"location" : @(RNPTypeLocation),
-                                @"camera" : @(RNPTypeCamera),
-                                @"microphone" : @(RNPTypeMicrophone),
-                                @"photo" : @(RNPTypePhoto),
-                                @"contacts" : @(RNPTypeContacts),
-                                @"event" : @(RNPTypeEvent),
-                                @"reminder" : @(RNPTypeReminder),
-                                @"bluetooth" : @(RNPTypeBluetooth),
-                                @"notification" : @(RNPTypeNotification),
-                                @"backgroundRefresh": @(RNPTypeBackgroundRefresh)
-                                }),
-                                RNPTypeUnknown, integerValue)
+RCT_ENUM_CONVERTER(RNPType, (@{
+    @"camera" : @(RNPTypeCamera),
+    @"photo" : @(RNPTypePhoto),
+}), RNPTypeUnknown, integerValue)
 
 @end
